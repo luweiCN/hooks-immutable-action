107cf1df80701d434d1c0450b0b479c86fae8d4d
luwei <1280380446@qq.com>
2020-03-30 16:43:47 +0800

    chapter2

--- src/App.js
+++ src/App.js
@@ -2,15 +2,22 @@ import React from "react";
 import "./App.css";
 import { GlobalStyle } from "./style";
 import { IconStyle } from "./assets/iconfont/iconfont";
+import { renderRoutes } from "react-router-config"; //renderRoutes 读取路由配置转化为 Route 标签
+import routes from "./routes/index.js";
+import { HashRouter } from "react-router-dom";
+import store from "./store/index";
+import { Provider } from "react-redux";
 
 function App() {
-    return (
-        <div className="App">
-            <GlobalStyle></GlobalStyle>
-            <IconStyle></IconStyle>
-            <i className="iconfont">&#xe62b;</i>
-        </div>
-    );
+  return (
+    <Provider store={store}>
+      <HashRouter>
+        <GlobalStyle></GlobalStyle>
+        <IconStyle></IconStyle>
+        {renderRoutes(routes)}
+      </HashRouter>
+    </Provider>
+  );
 }
 
 export default App;
