959ced906d16de119415d99ec8c56896a99a0ff2
luwei <1280380446@qq.com>
2020-04-10 15:59:06 +0800

    chapter3

--- src/application/Recommend/index.js
+++ src/application/Recommend/index.js
@@ -1,7 +1,66 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Slider from "../../components/slider";
+import Scroll from "../../baseUI/scroll/index";
+import RecommendList from "../../components/list";
+import { Content } from "./style";
+import * as actionTypes from "./store/actionCreators";
+import { connect } from "react-redux";
+import { forceCheck } from "react-lazyload";
+import Loading from "../../baseUI/loading/index";
 
 function Recommend(props) {
-  return <div>Recommend</div>;
+  const {
+    bannerList,
+    recommendList,
+    enterLoading,
+    getBannerDataDispatch,
+    getRecommendListDataDispatch
+  } = props;
+  useEffect(() => {
+    if (!bannerList.size) {
+      getBannerDataDispatch();
+    }
+    if (!recommendList.size) {
+      getRecommendListDataDispatch();
+    }
+    // eslint-disable-next-line
+  }, []);
+
+  return (
+    <Content>
+      <Scroll className="list" onScroll={forceCheck}>
+        <div>
+          <Slider bannerList={bannerList.toJS()}></Slider>
+          <RecommendList recommendList={recommendList.toJS()}></RecommendList>
+        </div>
+      </Scroll>
+      {enterLoading ? <Loading></Loading> : null}
+    </Content>
+  );
 }
 
-export default React.memo(Recommend);
+// 映射 Redux 全局的 state 到组件的 props 上
+const mapStateToProps = state => ({
+  // 不要在这里将数据 toJS
+  // 不然每次 diff 比对 props 的时候都是不一样的引用，还是导致不必要的重渲染，属于滥用 immutable
+  bannerList: state.getIn(["recommend", "bannerList"]),
+  recommendList: state.getIn(["recommend", "recommendList"]),
+  enterLoading: state.getIn(["recommend", "enterLoading"])
+});
+// 映射 dispatch 到 props 上
+const mapDispatchToProps = dispatch => {
+  return {
+    getBannerDataDispatch() {
+      dispatch(actionTypes.getBannerList());
+    },
+    getRecommendListDataDispatch() {
+      dispatch(actionTypes.getRecommendList());
+    }
+  };
+};
+
+// 将 ui 组件包装成容器组件
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(React.memo(Recommend));
